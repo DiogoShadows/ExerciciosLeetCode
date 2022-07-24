37c4008c7fe735b1fef1378e93a9337f4a3b9e28
Diogo Santos <diogo_f_s_@hotmail.com>
2022-07-24 12:17:20 -0300

    ReverseWordsInAStringIII_557 add

---
+++ Exercicios/ReverseWordsInAStringIII_557.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosLeetCode.Exercicios
+{
+    public class ReverseWordsInAStringIII_557
+    {
+        public static string Solucao(string s)
+        {
+            string resposta = "";
+            var words = s.Split(" ");
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                resposta += new string(words[i].Reverse().ToArray()) + " ";
+            }
+
+            return resposta.Trim();
+        }
+    }
+}
