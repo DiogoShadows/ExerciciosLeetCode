f88b93d853cf99ab106fe0be3ed14068acc0f7ce
Diogo Santos <diogo_f_s_@hotmail.com>
2022-08-15 17:21:47 -0300

    1472. Design Browser History

--- Program.cs
+++ Program.cs
@@ -7,7 +7,20 @@ namespace ExerciciosLeetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(RemoveOneElementtoMaketheArrayStrictlyIncreasing_1909.Solucao(new int[] { 1, 1, 1 }));
+            var a = new DesignBrowserHistory_1472("1");
+            a.Visit("2");
+            a.Visit("3");
+            Console.WriteLine(a.Back(9));
+            a.Visit("4");
+            Console.WriteLine(a.Forward(7));
+            a.Visit("5");
+            a.Visit("6");
+            Console.WriteLine(a.Forward(5));
+            a.Visit("7");
+            Console.WriteLine(a.Back(10));
+            a.Visit("8");
+            a.Visit("9");
+            Console.WriteLine(a.Forward(10));
         }
     }
 }
