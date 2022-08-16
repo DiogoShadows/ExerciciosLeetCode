69fe058ef3313535afec476ae436f42c24b1f9b6
Diogo Santos <diogo_f_s_@hotmail.com>
2022-08-16 14:28:39 -0300

    874. Walking Robot Simulation

--- Program.cs
+++ Program.cs
@@ -7,20 +7,7 @@ namespace ExerciciosLeetCode
     {
         static void Main(string[] args)
         {
-            var a = new DesignBrowserHistory_1472("1");
-            a.Visit("2");
-            a.Visit("3");
-            Console.WriteLine(a.Back(9));
-            a.Visit("4");
-            Console.WriteLine(a.Forward(7));
-            a.Visit("5");
-            a.Visit("6");
-            Console.WriteLine(a.Forward(5));
-            a.Visit("7");
-            Console.WriteLine(a.Back(10));
-            a.Visit("8");
-            a.Visit("9");
-            Console.WriteLine(a.Forward(10));
+            Console.WriteLine(WalkingRobotSimulation_874.Solucao(new int[] { 4, -1, 4, -2, 4 }, new int[][] { new int[] { 2, 4 } }));
         }
     }
 }
