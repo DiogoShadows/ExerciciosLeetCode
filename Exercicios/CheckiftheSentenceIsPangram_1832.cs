64601401a29314f5458ac526f4fecd556e275341
Diogo Santos <diogo_f_s_@hotmail.com>
2022-09-14 19:07:34 -0300

    1832. Check if the Sentence Is Pangram

---
+++ Exercicios/CheckiftheSentenceIsPangram_1832.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosLeetCode.Exercicios
+{
+    public class CheckiftheSentenceIsPangram_1832
+    {
+        public static bool Solucao(string sentence)
+        {
+            List<char> alfabeto = new List<char>{ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            foreach(char c in alfabeto)
+            {
+                if(!sentence.Contains(c))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ExerciciosLeetCode
             root.right = new TreeNode(2);
             root.right.left = new TreeNode(3);
 
-            Console.WriteLine(FindTargetIndicesAfterSortingArray_2089.Solucao(new int[] { 1, 2, 5, 2, 3 }, 2));
+            Console.WriteLine(CheckiftheSentenceIsPangram_1832.Solucao(""));
         }
     }
 }
