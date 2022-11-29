1466d87555b7957706e55338531a8857bedfb78b
Diogo Santos <diogo_f_s_@hotmail.com>
2022-11-29 20:54:20 -0300

    380. Insert Delete GetRandom O(1)

---
+++ Exercicios/InsertDeleteGetRandomO(1)_380.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ExerciciosLeetCode.Exercicios
+{
+    public class InsertDeleteGetRandomO_1__380
+    {
+        private static Dictionary<int, int> DicionaryNumbers;
+        private static Random RandomGenerator; 
+
+        public InsertDeleteGetRandomO_1__380()
+        {
+            DicionaryNumbers = new Dictionary<int, int>();
+            RandomGenerator = new Random();
+        }
+
+        public bool Insert(int val)
+        {
+            if (DicionaryNumbers.ContainsKey(val))
+                return false;
+
+            DicionaryNumbers.Add(val, val);
+            return true;
+        }
+
+        public bool Remove(int val) => DicionaryNumbers.Remove(val);
+
+        public int GetRandom() => DicionaryNumbers.ElementAt(RandomGenerator.Next(DicionaryNumbers.Count())).Key;
+    }
+}
