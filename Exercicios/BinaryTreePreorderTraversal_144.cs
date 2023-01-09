f840d9997cb8061ce3d81b64fed7b4de1b96180e
Diogo Santos <diogo_f_s_@hotmail.com>
2023-01-09 09:53:25 -0300

    144. Binary Tree Preorder Traversal

---
+++ Exercicios/BinaryTreePreorderTraversal_144.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ExerciciosLeetCode.Exercicios.MergeTwoBinaryTrees_617;
+
+namespace ExerciciosLeetCode.Exercicios
+{
+    public class BinaryTreePreorderTraversal_144
+    {
+        public static List<int> listNodes = new List<int>();
+        public static IList<int> PreorderTraversal(TreeNode root)
+        {
+            listNodes = new List<int>();
+            Recursion(root);
+            return listNodes;
+        }
+
+        public static void Recursion(TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            listNodes.Add(node.val);
+
+            Recursion(node.left);
+            Recursion(node.right);
+        }
+    }
+}
